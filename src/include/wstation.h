2ded5126344cd52e6c78fd84e528162b024c0ac1
Renê de Souza Pinto <rene@renesp.com.br>
2021-06-19 15:54:47 +0200

    Fix license copyright statement
    Fix text in all Copyright statements over the files.
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- src/include/wstation.h
+++ src/include/wstation.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* 
- * Copyright 2020 Renê de Souza Pinto
+ * Copyright (c) 2020 Renê de Souza Pinto. All rights reserverd.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
